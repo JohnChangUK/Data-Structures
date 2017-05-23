8b4eb4996c845b4ceccbab8b02144da4fd8811d2
John Chang <johnchang9094@gmail.com>
2017-05-23 22:45:48 +0100

    Kotlin Calculator

--- src/Operations.kt
+++ src/Operations.kt
@@ -4,9 +4,12 @@
 
 fun main(args:Array<String>){
 
-    var n1:Int= readLine()
-    var n2:Int=20
-    var sum:Int?
+    print("Enter n1:")
+    var n1:Float= readLine()!!.toFloat()
+
+    print("Enter n2")
+    var n2:Float=readLine()!!.toFloat()
+    var sum:Float?
     sum=n1+n2
     print("Sum: " + sum)
 
