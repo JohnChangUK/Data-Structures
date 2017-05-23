d3d30a4ed82c1d636e4fb18fad28437552934062
John Chang <johnchang9094@gmail.com>
2017-05-23 22:42:30 +0100

    Testing new Features

---
+++ src/DataTypes.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+
+    val name="John"
+    val age=22
+    val dep:String?
+    dep="Software"
+
+    println("name: " + name)
+    println("age: " + age)
+
+    val pi:Double=3.14
+    println("pi : " + pi)
+}
---
+++ src/Operations.kt
@@ -0,0 +1,13 @@
+/*
+    Math Calculator
+ */
+
+fun main(args:Array<String>){
+
+    var n1:Int= readLine()
+    var n2:Int=20
+    var sum:Int?
+    sum=n1+n2
+    print("Sum: " + sum)
+
+}
