f4ad17357882227220cfc04613ecb803fc3450ea
John Chang <johnchang9094@gmail.com>
2017-05-24 23:09:59 +0100

    Added functions to Class

--- src/CarClass.kt
+++ src/CarClass.kt
@@ -4,11 +4,15 @@ class Car(Type:String, Model:Int,
         var Owner:String?=null
         var Price:Double?=null
         var Miles:Int?=null
+        var Model:Int?=null
+        var Type:String?=null
 
         init {
             this.Owner = Owner
             this.Price = Price
             this.Miles = Miles
+            this.Model = Model
+            this.Type = Type
         }
 
     fun GetPrice():Double? {
@@ -16,8 +20,11 @@ class Car(Type:String, Model:Int,
     }
 
     fun GetOwner():String? {
-
         return this.Owner
+    }
+
+    fun GetType():String? {
+        return this.Type
     }
 }
 
@@ -26,8 +33,10 @@ fun main(args:Array<String>){
     var lambo = Car("Lambo", 2017, 10000.0, 100, "John")
     println("Owner:" + lambo.GetOwner())
     println("Price:" + lambo.GetPrice())
+    println("Type:" + lambo.GetType())
 
     var ferrari = Car("Ferrari", 2016, 5000.0, 100, "Frank")
     println("Owner:" + ferrari.GetOwner())
     println("Price:" + ferrari.GetPrice())
+    println("Type:" + ferrari.GetType())
 }
