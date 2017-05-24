f3323f3e353212cea031c46db50348c9ee842591
John Chang <johnchang9094@gmail.com>
2017-05-24 22:43:19 +0100

    Global Variables

---
+++ src/Scope.kt
@@ -0,0 +1,14 @@
+var fullName = "Jimmy Neutron"
+
+fun display():Unit{
+
+    println("fullName: $fullName")
+}
+
+fun main(args:Array<String>){
+    var name = "John"
+    println("fullName: $fullName")
+    println("name: $name")
+
+    display()
+}
