0a8fa2a74fefb442a794d77dedb23570730fe163
John Chang <johnchang9094@gmail.com>
2017-05-24 14:15:39 +0100

    Implemented Do While loop

---
+++ src/NestedForLoop.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by johnchang on 24/05/2017.
+ */
+fun main(args:Array<String>){
+
+    var count = 6
+
+    do {
+        println("Enter the loop")
+            if (count ==2) {
+                println("Count: $count")
+            }
+
+            count++
+
+    } while (count <= 5)
+    println("Loop Done")
+}
+
