8bba7eeff38d3d48d9dd06220a46e23873e48ef8
John Chang <johnchang9094@gmail.com>
2017-05-25 22:27:56 +0100

    Working with Packages

---
+++ src/CallPackage.kt
@@ -0,0 +1,9 @@
+// importing with * means EVERYTHING from that file
+import com.example.cars.*
+
+
+fun main(args:Array<String>){
+
+    var myClass = MyCar()
+    myClass.ShowInfo()
+}
---
+++ src/com/example/cars/MainClass.kt
@@ -0,0 +1,8 @@
+package com.example.cars
+
+class MyCar{
+
+    fun ShowInfo() {
+        println("BMW is a new car I bought ")
+    }
+}
