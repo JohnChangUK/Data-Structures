cc67aa9817ebd5a4522d0987fda68cd19d96e55d
John Chang <johnchang9094@gmail.com>
2017-05-26 11:48:50 +0100

    Reading from Files, File Reader and using When statement

--- src/MyFile.kt
+++ src/MyFile.kt
@@ -1,18 +1,45 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args:Array<String>) {
 
-    print("Write to File Text: ")
-    var str:String = readLine().toString()
-    WriteToFile(str)
+    println("1 - Read \n2 - Write \n")
+    val op = readLine()!!.toInt()
+
+    when (op){
+        1 -> ReadFromFile()
+
+        2 -> {
+            print("Write to File Text: ")
+            var str:String = readLine().toString()
+            WriteToFile(str)
+        }
+    }
+
 }
 
 fun WriteToFile(str:String){
     try {
-        var fo = FileWriter("test.txt", true)
+        val fo = FileWriter("test.txt", true)
         fo.write(str + "\n")
         fo.close()
+        print("Data is Saved!")
     } catch(ex: Exception){
+        print(ex.message)
+    }
+}
+
+fun ReadFromFile(){
+
+    try {
+        val fin = FileReader("test.txt")
+        var c:Int?
+        do {
+            c = fin.read()
+            print(c.toChar())
+        } while (c != -1)
+
+    } catch (ex:Exception) {
         print(ex.message)
     }
 }
